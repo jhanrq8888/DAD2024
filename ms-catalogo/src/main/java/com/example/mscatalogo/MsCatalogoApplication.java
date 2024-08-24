c77d02cb733e32ec1b46d4dabd90974004b534f0
jhanrq8888 <jhanramos8888@gmail.com>
2024-08-24 18:55:56 -0500

    :(

--- ms-catalogo/src/main/java/com/example/mscatalogo/MsCatalogoApplication.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/MsCatalogoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MsCatalogoApplication {
+public class  MsCatalogoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsCatalogoApplication.class, args);
